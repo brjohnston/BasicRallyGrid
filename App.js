17ee6fc0ee63d70249b98b84f0d9d7df35f04538
brjohnston <brooks.johnston@fmc-na.com>
2015-02-05 11:29:05 -0600

    My First App Working
    Completion of Building Rally Apps -- An Introduction to the 2.0 SDK

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function() {
-        console.log('Our first App woot!');
+	
+	launch: function() {
+		console.log('Our first App woot!');
 		
-		var myStore = Ext.create('Rally.data.wsapi.Store', {
-			model: 'User Story',
-			listeners: {
-				load: function(store, data, success) {
-					//process data
-				}
+		this._loadData();
+	},
+	
+	// Get data from Rally
+	_loadData: function() {
+	
+	var myStore = Ext.create('Rally.data.wsapi.Store', {
+		model: 'User Story',
+		autoLoad: true,
+		listeners: {
+			load: function(myStore, myData, success) {
+				console.log('got data!', myStore, myData,success);
+				this._loadGrid(myStore);
 			},
-			fetch: ['Name', 'ScheduleState']
-		});
-        //API Docs: https://help.rallydev.com/apps/2.0/doc/
-    }
+			scope: this
+		},
+		fetch: ['FormattedID','Name','ScheduleState']
+	});
+
+	},
+	
+	// Create and Show a Grid of given stories
+	_loadGrid: function(myStoryStore) {
+	var myGrid = Ext.create('Rally.ui.grid.Grid', {
+		store: myStoryStore,
+		columnCfgs: [
+			'FormattedID','Name','ScheduleState'
+		]
+	});
+	this.add(myGrid);
+	console.log('what is this?',this);
+	}
+	
 });
